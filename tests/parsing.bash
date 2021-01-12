25b7e0ee7c7a287af1f2b8575a796060b953856e
krambo <krambono@student.le-101.fr>
2021-01-12 15:33:59 +0100

    tests parsing

---
+++ tests/parsing.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+wait=true
+if [ "$1" == "--no-waiting" ] || [ "$2" == "--no-waiting" ]
+then
+	wait=false
+fi
+
+valgrind=false
+if [ "$1" == "--valgrind" ] || [ "$2" == "--valgrind" ]
+then
+	valgrind=true
+fi
+
+make
+for f in tests/parsing/files/* 
+do
+	echo -ne "\033[32m"
+	head -n 1 $f
+	echo -ne "\033[0m"
+	tail -n +2 $f
+	echo -e "\033[33m---------\033[0m"
+	if [ "$valgrind" == true ]
+	then
+		valgrind --leak-check=full -q ./n-puzzle $f
+	else
+		./n-puzzle $f
+	fi
+	if [ "$wait" == true ]
+	then
+		read
+	fi
+	echo
+done
